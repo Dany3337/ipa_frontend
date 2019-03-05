ffe814d53baf679112cf24272a025630abd0f06c
Dany <xbit.dany@gmail.com>
2019-03-05 15:25:33 +0100

    IPA Code Abgabe

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  constructor (private router: Router) {
+    this.checkCookie();
+  }
+
+  checkCookie() : void {
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
+  var access_token = getCookie('access_token');
+  if (!access_token) {
+    window.location.href="http://localhost:3000/auth/google";
+  } else {
+    var matches = access_token.match(/^s:(.+?)\./);
+    if (!matches) {
+      window.location.href="http://localhost:3000/auth/google";
+    } else {
+      console.log("Correct Access Token");
+    }
+    }
+  }
+
+}
